e64f2cb2299464dad9af8f2a40f6f944a64f0cd1
Ramzi Dreessen <ramzidreessen@gmail.com>
2019-04-17 20:21:08 -0500

    initial vue commit

---
+++ portfolio/src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Ramzi's Vue.js App"/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
