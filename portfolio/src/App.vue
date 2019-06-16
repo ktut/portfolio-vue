abc09f507e02b6508150d46ffef4a65b619e45f6
Ramzi Dreessen <ramzidreessen@gmail.com>
2019-06-16 18:54:45 -0500

    use correct font family for body text

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -346,9 +346,9 @@ export default {
     <br>
     <h2>Did the whole CMYK thing, too.</h2>
 
-    <p>My interest in web design stemmed from print, originally for the Chicago Sun-Times. I had many projects there, but two of the biggest were:</p>
-    <p>1. Designing a 103-page glossy embossed hardcover book for the Chicago Sun-Times Foundation in Fall/Winter 2015, including hundreds of photos shot over four years of event coverage</p>
-    <p>2. As a part of a three-person team, designed a 24-page glossy lifestyle magazine (SPLASH) from 2012-2016. Circulation: 225,000.</p>
+    <p>My interest in web design stemmed from print design, starting at the Chicago Sun-Times in 2012. Two of the biggest projects I worked on there were:</p>
+    <p><b>1.</b> Designing a 103-page glossy embossed hardcover book for the Chicago Sun-Times Foundation in Fall/Winter 2015, including hundreds of photos shot over four years of event coverage</p>
+    <p><b>2.</b> As a part of a three-person team, designed a 24-page glossy lifestyle magazine (SPLASH) from 2012-2016. Circulation: 225,000.</p>
 
     <button class="readmore" v-on:click="toggleSection('sectionPrintDesign')">
       {{ sectionPrintDesign.show ? 'Collapse Section -' : 'Expand Section +' }}
