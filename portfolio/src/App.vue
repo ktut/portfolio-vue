dbf90fd6af6b80acab3e434c617ba660f51b606b
Ramzi Dreessen <ramzidreessen@gmail.com>
2022-01-14 19:15:08 -0500

    Remove controls on hb first small vid

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -521,7 +521,7 @@ export default {
 
      <!-- v-bind:class="[selectedCategory]" -->
   <section class="hb" style="max-width: 900px; margin: 0 auto;" id="code">
-    <video v-bind:class="`video lazy hb-mobile small first ${[sectionHb.show]}`"  width="100%" autoplay loop muted playsinline controls src="https://ktut.github.io/portfolio/assets/rendered/HB-mobile-2.mp4"></video>
+    <video v-bind:class="`video lazy hb-mobile small first ${[sectionHb.show]}`"  width="100%" autoplay loop muted playsinline src="https://ktut.github.io/portfolio/assets/rendered/HB-mobile-2.mp4"></video>
     <h2>I enjoy being the utility guy.</h2>
     <p>I’m no stranger to acting as a one-man agency for the right project. For Chicago-based real estate company Hudson Burnham, they wanted a homepage that would attract attention and play nicely with their slogan "Move Up". They wanted a video background unlike any of their competitors - stock footage wouldn't cut it.</p> 
 
